988b8984642df4f44268cb6fd013b32f62baf6f5
velsthinez <choonyung@gmail.com>
2023-10-30 16:25:11 +0800

    added different firemodes to weapon class

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public enum FireModes
+    {
+        Auto, // = 0
+        SingleFire, // = 1
+        BurstFire // = 2
+
+    }
+
+    public FireModes FireMode;
+    
     public GameObject Projectile;
     public Transform SpawnPos;
     public float Interval = 0.1f;
-    
+
     private float _timer = 0f;
     private bool _canShoot = true;
+    private bool _singleFireReset = true;
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         if (_timer < Interval)
         {
@@ -30,19 +42,50 @@ public class Weapon : MonoBehaviour
         {
             Debug.LogWarning("Missing Projectile prefab");
             return;
-        }        
-        
+        }
+
         if (SpawnPos == null)
         {
             Debug.LogWarning("Missing SpawnPosition transform");
             return;
         }
 
+        switch (FireMode)
+        {
+            case (FireModes.SingleFire):
+            {
+                SingleFireShoot();
+                break;
+            }
+
+            case (FireModes.Auto):
+            {
+                AutoFireShoot();
+                break;
+            }
+        }
+      
+    }
+
+    void AutoFireShoot()
+    {
         if (!_canShoot)
+            return;
+
+        GameObject.Instantiate(Projectile, SpawnPos.position, SpawnPos.rotation);
+    }
+
+    void SingleFireShoot()
+    {
+        if (!_singleFireReset)
             return;
         
         GameObject.Instantiate(Projectile, SpawnPos.position, SpawnPos.rotation);
-
+        _singleFireReset = false;
     }
-    
+
+    public void StopShoot()
+    {
+        _singleFireReset = true;
+    }
 }
